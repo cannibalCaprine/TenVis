4f070d583748f777582584f7bccf8f5f37382a9d
Septicake <septicake056@gmail.com>
2024-11-19 00:39:04 -0500

    Jumping has been someone hastily set up

--- game.gd
+++ game.gd
@@ -9,4 +9,4 @@ extends Node2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	$Label.text  = "health: %.2f\nspeed: %f\nx vel: %f\n" % [$Player.health, $Player.accel, $Player.velocity.x]
-	$Label.text += "x skid: %s" % [$Player.x_skid]
+	$Label.text += "y vel: %s\njumping: %s\nskidding: %s" % [$Player.velocity.y, $Player.jumping, $Player.x_skid]
--- player.gd
+++ player.gd
@@ -14,12 +14,17 @@ var accel: float: # linearly interpolate based on health
 var max_speed: float = 600
 var min_skid: float = 400
 
-var grav_accel: float = 5;
-var max_fall: float = 400
-var max_up: float = 100 #arbitrary for the time being
+var jump_force: float = -625
+var jumping = false
+
+var grav_accel: float = 3;
+var max_fall: float = 800
+
+var original_pos: Vector2
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	original_pos = Vector2(position)
 	pass # Replace with function body.
 
 
@@ -32,7 +37,7 @@ func _physics_process(_delta: float) -> void:
 	get_input();
 	velocity.x = clampf(velocity.x, -max_speed, max_speed)
 	velocity.y += 20
-	velocity.y = clampf(velocity.y, max_fall, max_up)
+	velocity.y = clampf(velocity.y, -INF, max_fall)
 	
 	# round it down
 	if(Global.basically_zero(velocity.x, 1)):
@@ -42,6 +47,9 @@ func _physics_process(_delta: float) -> void:
 		x_skid = false
 		
 	move_and_slide();
+	
+	if jumping && is_on_floor():
+		jumping = false
 	
 func get_input() -> void:
 	var curr_accel = lerpf(accel, 0, velocity.length() / max_speed)
@@ -54,11 +62,20 @@ func get_input() -> void:
 		#velocity.x += accel
 		x_dir += 1
 		
+	if Input.is_action_just_pressed("ui_up") && !jumping:
+		velocity.y += jump_force
+		jumping = true
+		
 	if signf(x_dir) != signf(velocity.x) and velocity.length() > min_skid:
 		x_skid = true
+	if signf(x_dir) == signf(velocity.x) || !is_on_floor():
+		x_skid = false
 	
 	if signf(x_dir) != signf(velocity.x) && !x_skid:
 		curr_accel *= 1.5
+		
+	if !is_on_floor():
+		curr_accel *= 1.25
 		
 	var x_accel = curr_accel * x_dir
 			
@@ -78,3 +95,6 @@ func get_input() -> void:
 		health += 0.5
 	if Input.is_key_pressed(KEY_Q):
 		health -= 0.5
+		
+	if Input.is_key_pressed(KEY_R):
+		position = Vector2(original_pos)
